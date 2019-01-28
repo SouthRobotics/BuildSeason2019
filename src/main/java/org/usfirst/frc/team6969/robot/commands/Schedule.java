33953d0afe539f3d8ce501e1afa57e6deb792daf
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2019-01-28 16:11:31 -0500

    removed unnecessary imports

--- src/main/java/org/usfirst/frc/team6969/robot/commands/Schedule.java
+++ src/main/java/org/usfirst/frc/team6969/robot/commands/Schedule.java
@@ -1,19 +1,6 @@
 package org.usfirst.frc.team6969.robot.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import org.usfirst.frc.team6969.robot.subsystems.*;
-import org.usfirst.frc.team6969.robot.Robot;
-import org.usfirst.frc.team6969.robot.RobotMap;
-import org.usfirst.frc.team6969.robot.commands.RotateToAngle;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Schedule extends CommandGroup{
     public Schedule() {
