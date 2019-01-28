01b3df6584c747375fd1ede64de89c900f9d7836
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2019-01-28 14:44:33 -0500

    Got rid of unnecessary variables and lines

--- src/main/java/org/usfirst/frc/team6969/robot/subsystems/DriveTrain.java
+++ src/main/java/org/usfirst/frc/team6969/robot/subsystems/DriveTrain.java
@@ -19,20 +19,13 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class DriveTrain extends Subsystem {
 
 	private static DifferentialDrive robotDrive;
-	private static boolean goHalfSpeed;
-	private static boolean goFullSpeed;
 	private static int leftYAxis;
 	private static int rightYAxis;
 	
     public void initDefaultCommand() {
     	robotDrive =  RobotMap.drive;
-        goHalfSpeed = false;
-        goFullSpeed = false;
         leftYAxis = Robot.m_oi.leftYAxis;
 		rightYAxis = Robot.m_oi.rightYAxis;
-		robotDrive =  RobotMap.drive;
-        goHalfSpeed = false;
-		goFullSpeed = false;
         setDefaultCommand(new TeleOpDrive());
     }	
 }
