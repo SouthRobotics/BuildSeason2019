e21304d8f024252d3d89434c63e0a102190f00d5
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2019-01-28 16:14:59 -0500

    renamed schedule example

--- src/main/java/org/usfirst/frc/team6969/robot/commands/ExampleSchedule.java
+++ src/main/java/org/usfirst/frc/team6969/robot/commands/ExampleSchedule.java
@@ -2,8 +2,8 @@ package org.usfirst.frc.team6969.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class Schedule extends CommandGroup{
-    public Schedule() {
+public class ExampleSchedule extends CommandGroup {
+    public ExampleSchedule() {
         addSequential(new moveBotFast());
         addSequential(new moveBotSlow());
         addSequential(new moveBotFast());
