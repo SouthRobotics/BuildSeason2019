bc974c700f3ed99f472fcff450c213136d33ae2f
SouthRobotics <southrobotics@gmail.com>
2019-02-08 12:09:28 -0500

    updated wpilib version

--- src/main/java/org/usfirst/frc/team6969/robot/OI.java
+++ src/main/java/org/usfirst/frc/team6969/robot/OI.java
@@ -84,7 +84,7 @@ public class OI {
     public OI() {
 		rButton2.whenPressed(new ManualOverride());
 		lButton3.whenPressed(new RotateToPixyTarget(Robot.pixyCenter));
-		lButton4.whenPressed(new RotateChassisToAngle(90));
+		lButton4.whenPressed(new RotateChassisToAngle(-90));
 		lButton5.whenPressed(new RotateChassisToAnglePID(-90));
 		rButton5.whileHeld(new SpinRollerIntake(true));
 		rButton6.whileHeld(new SpinRollerIntake(false));
