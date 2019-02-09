afd0006595b7e0cfc435d931cfc99deae10bc926
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2019-02-09 14:50:36 -0500

    moved PID to arm

--- src/main/java/org/usfirst/frc/team6969/robot/subsystems/Arm.java
+++ src/main/java/org/usfirst/frc/team6969/robot/subsystems/Arm.java
@@ -14,6 +14,8 @@ import javax.lang.model.util.ElementScanner6;
 import org.usfirst.frc.team6969.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -22,7 +24,7 @@ import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 /**
  * 2019 build season
  */
-public class Arm extends Subsystem {
+public class Arm extends Subsystem implements PIDOutput{
 
     private static Spark rotatingPlatformMotor;
     private static Spark bottomMotor;
@@ -35,19 +37,49 @@ public class Arm extends Subsystem {
     public static PIDController bottomAnglePID;
     public static PIDController middleAnglePID;
     public static PIDController topAnglePID;
+    private int[][] pidVals = {{0,0,0},
+                            {0,0,0},
+                            {0,0,0}};
+    public static int pidSpeed;
 
-    public void initDefaultCommand() {
+    public Arm()
+    {
         rotatingPlatformMotor = RobotMap.rotatingPlatformMotor;
         bottomMotor = RobotMap.bottomMotor;
         middleMotor = RobotMap.middleMotor;
         topMotor = RobotMap.topMotor;
-        bottomPotentiometer = RobotMap.bottomPotentiometer;
-        middlePotentiometer = RobotMap.middlePotentiometer;
-        topPotentiometer = RobotMap.topPotentiometer;
+        bottomPotentiometer = RobotMap.bottomJointPot;
+        middlePotentiometer = RobotMap.middleJointPot;
+        topPotentiometer = RobotMap.topJointPot;
         rotatingPlatformEncoder = RobotMap.rotatingPlatformEncoder;
+        initPIDControllers();
+    }
+    
+    public void initDefaultCommand() {
+
         setDefaultCommand(null);
-        bottomAnglePID = new PIDController()
-	}
+    }
+    
+    public void initPIDControllers() 
+    {
+        bottomAnglePID = new PIDController(pidVals[0][0], pidVals[0][1], pidVals[0][2], bottomPotentiometer, this);    //pid values need tuning, especially for smaller angles!
+        bottomAnglePID.setInputRange(0.0, 360.0);
+        bottomAnglePID.setOutputRange(-0.6, 0.6);  // don't need to rotate extremely fast
+        bottomAnglePID.setAbsoluteTolerance(1);  // 2 degree threshold
+        bottomAnglePID.setContinuous(true);
+
+        middleAnglePID = new PIDController(pidVals[1][0], pidVals[1][1], pidVals[1][2], middlePotentiometer, this);    //pid values need tuning, especially for smaller angles!
+        middleAnglePID.setInputRange(0.0, 360.0);
+        middleAnglePID.setOutputRange(-0.6, 0.6);  // don't need to rotate extremely fast
+        middleAnglePID.setAbsoluteTolerance(1);  // 2 degree threshold
+        middleAnglePID.setContinuous(true);
+
+        topAnglePID = new PIDController(pidVals[2][0], pidVals[2][1], pidVals[2][2], topPotentiometer, this);    //pid values need tuning, especially for smaller angles!
+        topAnglePID.setInputRange(0.0, 360.0);
+        topAnglePID.setOutputRange(-0.6, 0.6);  // don't need to rotate extremely fast
+        topAnglePID.setAbsoluteTolerance(1);  // 2 degree threshold
+        topAnglePID.setContinuous(true);
+    }
     
     public void rotate(int joint, double speed) {  
         if (joint == 0)
@@ -58,5 +90,10 @@ public class Arm extends Subsystem {
             middleMotor.set(speed);
         else
             topMotor.set(speed);
+    }
+
+    @Override
+    public void pidWrite(double output) {
+        
     }
 }
