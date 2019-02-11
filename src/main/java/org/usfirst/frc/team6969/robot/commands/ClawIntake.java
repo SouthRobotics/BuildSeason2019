fca02664fda83ed3b12fb41946d7d0e73da77788
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2019-02-11 15:02:08 -0500

    uodated claw methods

--- src/main/java/org/usfirst/frc/team6969/robot/commands/ClawIntake.java
+++ src/main/java/org/usfirst/frc/team6969/robot/commands/ClawIntake.java
@@ -26,7 +26,7 @@ public class ClawIntake extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.claw.in();
+    Robot.claw.spinIn();
   }
 
   // Make this return true when this Command no longer needs to run execute()
