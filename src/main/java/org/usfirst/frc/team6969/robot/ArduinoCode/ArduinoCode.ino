3095e878fd017524fab254ca2ba6f149a723deb2
maxtheeverything7 <maksim.peter777@gmail.com>
2019-03-22 12:06:17 -0400

    dfdf

--- src/main/java/org/usfirst/frc/team6969/robot/ArduinoCode/ArduinoCode.ino
+++ src/main/java/org/usfirst/frc/team6969/robot/ArduinoCode/ArduinoCode.ino
@@ -44,7 +44,6 @@ const byte ultrasonicValue = 0x13;*/
  *  Public Domain
  */
 
-const byte ultrasonicValue = 0x13; //can be anything just needs to match in RoboRio code
 
 // Pins
 const int TRIG_PIN = 7;
@@ -127,40 +126,8 @@ void loop() {
 
 //SparkFun Ultrasonic HC-SR04 Sensor --------------------------------------------------------
 
-    if ( valueFromRoboRio == ultrasonicValue )
-      {
-      unsigned long t1;
-      unsigned long t2;
-      unsigned long pulse_width;
-      float cm;
-      float inches;
     
-      // Hold the trigger pin high for at least 10 us
-      digitalWrite(TRIG_PIN, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TRIG_PIN, LOW);
     
-      // Wait for pulse on echo pin
-      while ( digitalRead(ECHO_PIN) == 0 );
-    
-      // Measure how long the echo pin was held high (pulse width)
-      // Note: the micros() counter will overflow after ~70 min
-      t1 = micros();
-      while ( digitalRead(ECHO_PIN) == 1);
-      t2 = micros();
-      pulse_width = t2 - t1;
-    
-      // Calculate distance in centimeters and inches. The constants
-      // are found in the datasheet, and calculated from the assumed speed 
-      //of sound in air at sea level (~340 m/s).
-      cm = pulse_width / 58.0;
-      inches = pulse_width / 148.0;
-
-      if ( pulse_width > MAX_DIST )
-        Serial.print("-1");
-      else
-        Serial.print(cm);
-    }
 //Both --------------------------------------------------------
   delay(60);  // in ms, gives computer time to process data
  
